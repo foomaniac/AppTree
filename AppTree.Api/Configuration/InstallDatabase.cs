c976b09397ecdb1f0988de17ae6868e4bbad531c
Dave Jones <foomaniac@gmail.com>
2021-06-21 20:58:00 +0100

    Initial api project + swagger

---
+++ AppTree.Api/Configuration/InstallDatabase.cs
@@ -0,0 +1,15 @@
+﻿using AppTree.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppTree.Api.Configuration
+{
+    public static class InstallDatabase
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection @this, IConfiguration configuration)
+        {
+            @this.AddPersistance(configuration);
+            return @this;
+        }
+    }
+}
---
+++ AppTree.Api/Configuration/InstallSwagger.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace AppTree.Api.Configuration
+{
+    public static class InstallSwagger
+    {
+       public static IServiceCollection AddSwagger(this IServiceCollection @this)
+        {
+            @this.AddSwaggerGen(opt => opt.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "AppTree Api",
+                Description = "AppTree Supporting Api"
+               
+            }));
+            return @this;
+        }
+    }
+}
