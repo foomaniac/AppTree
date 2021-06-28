7b64ed2b4632c7a975abf76d0a99181b76d285b8
Dave Jones <foomaniac@gmail.com>
2021-06-28 23:12:41 +0100

    enabled cors rules

---
+++ AppTree.Api/Configuration/InstallCors.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppTree.Api.Configuration
+{
+    public static class InstallCors
+    {
+        public static void AddCorsConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            var corsBuilder = new CorsPolicyBuilder();
+
+            //corsBuilder.AllowAnyOrigin();// For anyone access. // doesn't work
+            corsBuilder.WithOrigins(
+                configuration["WhitelistedOrigin"].Split(",")
+            );
+            corsBuilder.AllowAnyHeader();
+            corsBuilder.AllowCredentials();
+            corsBuilder.AllowAnyMethod();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("SiteCorsPolicy", corsBuilder.Build());
+            });
+        }
+    }
+}
